34b390dbc0fe3be84425c28c7fdce53a1386b8d6
GoodDoctor05 <shanesrf@gmail.com>
2014-07-22 16:10:56 -0700

    all 3 ajax called made, parsed and displayed on view

--- public/js/main.js
+++ public/js/main.js
@@ -16,4 +16,36 @@ $( document ).ready(function() {
                                             )};
                                 }
             })
+    $.ajax({
+        url: "http://api.nytimes.com/svc/elections/us/v3/finances/2014/candidates/leaders/receipts-total.json?api-key=c353cbc0ae7d858a504f6ed663c0a326:5:69483126",
+        //force to handle it as jsonp by adding 'callback='
+        dataType: "jsonp",
+        success: function(data) {                   
+                for (var key in data.results) {
+                    $('#twentytotal').append('<tr id="' + data.results[key].party + '">' +
+                                                '<td>' + data.results[key].name  + '</td>' + 
+                                                '<td>' + data.results[key].party + '</td>' +
+                                                '<td>' + data.results[key].total_contributions + '</td>' +
+                                                '<td>' + data.results[key].date_coverage_from + '</td>' +
+                                                '<td>' + data.results[key].date_coverage_to + '</td>' +
+                                            '</tr>'
+                                            )};
+                                }
+            })
+    $.ajax({
+        url: "http://api.nytimes.com/svc/elections/us/v3/finances/2014/candidates/leaders/individual-total.json?api-key=c353cbc0ae7d858a504f6ed663c0a326:5:69483126",
+        //force to handle it as jsonp by adding 'callback='
+        dataType: "jsonp",
+        success: function(data) {                   
+                for (var key in data.results) {
+                    $('#twentyindividualtotal').append('<tr id="' + data.results[key].party + '">' +
+                                                '<td>' + data.results[key].name  + '</td>' + 
+                                                '<td>' + data.results[key].party + '</td>' +
+                                                '<td>' + data.results[key].total_from_individuals + '</td>' +
+                                                '<td>' + data.results[key].date_coverage_from + '</td>' +
+                                                '<td>' + data.results[key].date_coverage_to + '</td>' +
+                                            '</tr>'
+                                            )};
+                                }
+            })
 });
